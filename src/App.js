1037e5c1822d1235f807684a614d0c3419a637c1
sadikhossain69 <sadikhossain433@gmail.com>
2022-05-09 20:50:59 +0600

    creating a beautifull card

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './image/Screenshot_2.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='h-screen flex items-center justify-center bg-gray-100'>
+      <div className='max-w-sm mx-auto p-8 bg-white rounded-xl shadow-md space-y-2' >
+        <img className='h-24 mx-auto rounded-sm ring-1 ring-green-200' src={logo} alt="" />
+        <div className='text-center space-y-2'>
+          <div className='space-y-2'>
+            <p className='text-lg text-black font-semibold' >Learn With Sumit</p>
+            <p className='text-gray-500 font-medium'>YouTube Channel</p>
+          </div>
+          <button className='px-4 py-1 border border-purple-200 rounded-full font-semibold text-sm text-purple-600' >
+            Visit Youtube
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
